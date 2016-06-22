ccd0e7cd5a8bee88ead944bf3ce037d0f5e0eff7
Logan McAnsh <l.m@loganmcansh.com>
2016-06-22 17:42:58 -0400

    fade and zoom on scroll :check:
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -9,27 +9,18 @@ $(window).on('scroll', function () {
     offset = offset + height / 1.5;
     var calc = 1 - (scrollTop - offset + range) / range;
 
+
+    var size = (scrollTop/3000)+1;
     header.css({ 'opacity': calc });
+    header.css('transform', 'scale('+size+')');
+
 
     if ( calc > '1' ) {
-      header.css({ 'opacity': 1 });
+      header.css( 'opacity', '1' );
     } else if ( calc < '0' ) {
-      header.css({ 'opacity': 0 });
+      header.css( 'opacity', '0' );
     }
 
     // $('nav').toggleClass('scroll');
 
 });
-
-
-
-// $().ready(function() {
-//   var $fadeaway = $("header .container h1");
-//
-//   $(window).scroll(function(){
-//     $fadeaway
-//       .stop()
-//       // .animate({"marginTop": ($(window).scrollTop() + 0) + "px"}, "0" );
-//       $("header .container h1").animate({margin: "200px 0px 0px 0px"}, 100);
-//   });
-// });
