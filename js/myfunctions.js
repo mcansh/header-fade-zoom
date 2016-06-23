625f188b402dadcfd3b817149f55c685ae72ed4a
Logan McAnsh <l.m@loganmcansh.com>
2016-06-23 18:26:09 -0400

    smoother animation
    Signed-off-by: Logan McAnsh <l.m@loganmcansh.com>

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,46 +1,56 @@
-var header = $('.container');
-var range = 200;
-var pContainerHeight = $('header .container').height();
-var width = $(window).width();
+$(document).ready(function() {
 
-$(window).scroll(function(){
+  var header = $('header');
+  var header_img = $('header .bg');
+  var range = 200;
+  var pContainerHeight = $('header').height();
+  var width = $(window).width();
+  var about = $('#about');
 
-    var wScroll = $(this).scrollTop();
-    var offset = header.offset().top;
-    var height = header.outerHeight();
-    offset = offset + height / 1.5;
-    var calc = 1 - (wScroll - offset + range) / range;
+  $(window).scroll(function(){
+
+      var wScroll = $(this).scrollTop();
+      var offset = header.offset().top;
+      var height = header.outerHeight();
+      var $window = $(window);
+      offset = offset + height / 1.5;
+      var calc = 1 - (wScroll - offset + range) / range;
+
+      $window.scroll(function() {
+        if ($window.scrollTop() >= about.top) {
+            $("nav").addClass("scroll");
+        }
+      });
 
 
-    var size = (wScroll/3000)+1;
-    header.css({ 'opacity': calc });
-    header.css('transform', 'scale('+size+')');
+      var size = (wScroll/3000)+1;
+      header_img.css({ 'opacity': calc });
+      header_img.css('transform', 'scale('+size+')');
 
 
-    if ( calc > '1' ) {
-      header.css( 'opacity', '1' );
-      $('nav').removeClass('scroll');
-    } else if ( calc < '0' ) {
-      header.css( 'opacity', '0' );
-      $('nav').addClass('scroll');
-    }
-
-    if ((width >= 800)) {
-      if (wScroll <= pContainerHeight) {
-
-        $('header .container h1').css({
-          'transform' : 'translate(0px, '+ wScroll /2 +'%)'
-        });
+      if ( calc > '1' ) {
+        header_img.css( 'opacity', '1' );
+      } else if ( calc < '0' ) {
+        header_img.css( 'opacity', '0' );
       }
-    }
 
-});
+      if ((width >= 800)) {
+        if (wScroll <= pContainerHeight) {
+
+          $('header h1').css({
+            'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+          });
+        }
+      }
+
+  });
 
 
-var $hamburger = $(".hamburger");
-$hamburger.on("click", function(e) {
-  $hamburger.toggleClass("is-active");
-  // Do something else, like open/close menu
-  $('nav.mobile').toggleClass('open');
-  $('body').toggleClass('fixed');
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function() {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+    $('nav.mobile').toggleClass('open');
+    $('body').toggleClass('fixed');
+  });
 });
